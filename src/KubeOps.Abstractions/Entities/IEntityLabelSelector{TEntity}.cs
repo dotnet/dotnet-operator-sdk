51431e926a572179b038e8b25470de84f6fc4e78
Marcus Kimpenhaus <kimpenhaus@devil-engineering.de>
2026-06-12 12:20:58 +0200

    chore: removed more warnings

--- src/KubeOps.Abstractions/Entities/IEntityLabelSelector{TEntity}.cs
+++ src/KubeOps.Abstractions/Entities/IEntityLabelSelector{TEntity}.cs
@@ -7,9 +7,6 @@ using k8s.Models;
 
 namespace KubeOps.Abstractions.Entities;
 
-// This is the same pattern used by Microsoft on ILogger<T>.
-// An alternative would be to use a KeyedSingleton when registering this however that's only valid from .NET 8 and above.
-// Other methods are far less elegant
 #pragma warning disable S2326
 public interface IEntityLabelSelector<TEntity>
     where TEntity : IKubernetesObject<V1ObjectMeta>
